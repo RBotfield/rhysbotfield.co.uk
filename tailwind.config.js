f49d3c373e270b2404ac77b8a160862fee56f389
Rhys Botfield <rhys4995@live.co.uk>
2020-05-18 03:47:58 +0100

    Add back original TailwindCSS gray as cool-gray, let normal text fallback to body color, and lighten headers/bolds

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,19 @@ module.exports = {
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'cool-gray': {
+                    100: '#f7fafc',
+                    200: '#edf2f7',
+                    300: '#e2e8f0',
+                    400: '#cbd5e0',
+                    500: '#a0aec0',
+                    600: '#718096',
+                    700: '#4a5568',
+                    800: '#2d3748',
+                    900: '#1a202c',
+                },
+            },
         },
         screens: {
             xs: '375px',
